47159ce7160e303f5e6c9fe24929c63a39d30258
efs-multiconsult <efs@multiconsult.no>
2022-11-28 21:43:47 +0100

    Addet ny rad, Tor

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ const users = [
   { brukernavn: "Erlend Førde Schjetne", alder: 29, empYr: 2019 , bildeUrl: "images/efs.jpg" },
   { brukernavn: "Ragnhild Johanne Tunheim", alder: 31, empYr: 2022 , bildeUrl: "images/rjt.jpg" },
   { brukernavn: "Hans Ole Løvland", alder: 67, empYr: 2016 , bildeUrl: "images/hol.jpg" },
-  { brukernavn: "Inger Lise Gjerdebakken", alder: 40, empYr: 2021 , bildeUrl: "images/ingl.jpg" }
+  { brukernavn: "Inger Lise Gjerdebakken", alder: 40, empYr: 2021 , bildeUrl: "images/ingl.jpg" },
+  { brukernavn: "Tor Andersen", alder: 60, empYr: 1979 , bildeUrl: "images/toa.jpg" }
 ];
 
 function App() {
